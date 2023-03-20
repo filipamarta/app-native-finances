cf7c690d5a8618b73daff6d3f0de145218ad3642
Filipa <filipa.m@tamanna.com>
2023-03-20 12:52:47 +0000

    create colors as contants

---
+++ contants/colors.js
@@ -0,0 +1,14 @@
+const Colors = {
+  primary100: "#F1FAEE",
+  primary300: "#A8DADC",
+  primary400: "#7d9c9d",
+  primary500: "#457B9D",
+  primary600: "#1D3557",
+  primary700: "#061832",
+  secondary500: "#EF8354",
+  tertiary500: "#C96480",
+  neutral400: "#d1cece",
+  neutral500: "#a3a0a0",
+};
+
+export default Colors;
