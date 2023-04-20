647556c65ca04aee36ada30c338cbf763e63e2d0
Filipa <filipa.m@tamanna.com>
2023-04-20 13:46:51 +0100

    app

--- App.js
+++ App.js
@@ -1,28 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ExpensesContextProvider, {
   useExpenses,
 } from './context/ExpensesContext';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import AllExpenses from './screens/AllExpenses';
-import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/expenses/AllExpenses';
+import RecentExpenses from './screens/expenses/RecentExpenses';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ManageExpense from './screens/ManageExpense';
+import ManageExpense from './screens/expenses/ManageExpense';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import IconButton from './components/ui/IconButton';
 import Colors from './contants/colors';
 import { getExpensesApi } from './services/expensesApi';
 import LoadingOverlay from './components/ui/LoadingOverlay';
 import ErrorOverlay from './components/ui/ErrorOverlay';
+import LoginScreen from './screens/auth/LoginScreen';
+import SignupScreen from './screens/auth/SignupScreen';
+import LogoutScreen from './screens/auth/LogoutScreen';
+import AuthContextProvider, { useAuth } from './context/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const AuthStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={() => ({
+        headerStyle: {
+          backgroundColor: Colors.primary300,
+        },
+        headerTintColor: Colors.primary600,
+        contentStyle: { backgroundColor: Colors.tertiary500 },
+      })}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+};
+
 const ExpensesOverview = () => {
+  const { token } = useAuth();
   const manageExpenseHandler = (navigation) =>
     navigation.navigate('ManageExpense');
+
+  const logoutHandler = (navigation) => navigation.navigate('LogoutScreen');
 
   const { setExpenses } = useExpenses();
   const [isFetchingDataFromApi, setIsFetchingDataFromApi] = useState(true);
@@ -31,7 +56,7 @@ const ExpensesOverview = () => {
   const getExpenses = async () => {
     try {
       setIsFetchingDataFromApi(true);
-      const expenses = await getExpensesApi();
+      const expenses = await getExpensesApi(token);
       setExpenses(expenses);
       setIsFetchingDataFromApi(false);
     } catch (error) {
@@ -73,6 +98,16 @@ const ExpensesOverview = () => {
         tabBarLabelStyle: { fontSize: 12 },
         tabBarActiveTintColor: Colors.primary600,
         tabBarInactiveTintColor: Colors.primary400,
+        headerLeft: () => (
+          <View style={styles.logoutContainer}>
+            <IconButton
+              iconName="settings-outline"
+              iconColor={Colors.primary700}
+              iconSize={30}
+              onPress={() => logoutHandler(navigation)}
+            />
+          </View>
+        ),
         headerRight: () => (
           <View style={styles.iconContainer}>
             <IconButton
@@ -111,37 +146,83 @@ const ExpensesOverview = () => {
   );
 };
 
+const ExpensesScreens = () => {
+  return (
+    <>
+      <Stack.Navigator
+        screenOptions={() => ({
+          headerStyle: {
+            backgroundColor: Colors.primary300,
+          },
+          headerTintColor: Colors.primary600,
+        })}
+      >
+        <Stack.Screen
+          name="ExpensesOverview"
+          component={ExpensesOverview}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ManageExpense"
+          component={ManageExpense}
+          options={{ presentation: 'modal' }}
+        />
+        <Stack.Screen
+          name="LogoutScreen"
+          component={LogoutScreen}
+          options={{ presentation: 'modal' }}
+        />
+      </Stack.Navigator>
+    </>
+  );
+};
+
+const Navigation = () => {
+  const { isAuthenticated } = useAuth();
+  return (
+    <NavigationContainer>
+      {!isAuthenticated && <AuthStack />}
+      {isAuthenticated && <ExpensesScreens />}
+    </NavigationContainer>
+  );
+};
+
+const Root = () => {
+  /* 
+  HACK to have access to auth context 
+  and use getData func from there to get the token.
+  With this we don't pass throught LOGINScreen anymore
+  */
+  const { authenticate } = useAuth();
+
+  const fetchToken = async () => {
+    const storedToken = await AsyncStorage.getItem('token');
+    if (storedToken !== null) {
+      authenticate(storedToken);
+    }
+  };
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+
+  return <Navigation />;
+};
+
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <ExpensesContextProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={() => ({
-              headerStyle: {
-                backgroundColor: Colors.primary300,
-              },
-              headerTintColor: Colors.primary600,
-            })}
-          >
-            <Stack.Screen
-              name="ExpensesOverview"
-              component={ExpensesOverview}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="ManageExpense"
-              component={ManageExpense}
-              options={{ presentation: 'modal' }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ExpensesContextProvider>
+      <AuthContextProvider>
+        <ExpensesContextProvider>
+          <Root />
+        </ExpensesContextProvider>
+      </AuthContextProvider>
     </>
   );
 }
 
 const styles = StyleSheet.create({
   iconContainer: { marginRight: 16 },
+  logoutContainer: { marginLeft: 16 },
 });
